60f0dfa13dbde9ed08a208f52cbb22133482399e
divyashasinha23 <divyashasinha23@gmail.com>
2021-02-14 16:01:20 +0530

    updated

--- routes/partnerRoute.js
+++ routes/partnerRoute.js
@@ -68,68 +68,10 @@ router.post('/partnerwithus',(req,res) => {
                             
                         }
                     });
-            path = req.file.path
-            var transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                  user: process.env.USER,
-                  pass: process.env.PASS
-                }
-              });
-              
-              const output = `
-                    <p>You have a new contact request</p>
-                    <h3>Contact Details</h3>
-                    <ul>  
-                      <li>Outlet Name: ${req.body.outletName}</li>
-                      <li>State: ${req.body.state}</li>
-                      <li>City: ${req.body.city}</li>
-                      <li>Primary Location: ${req.body.primary_location}</li>
-                      <li>Pincode: ${req.body.pincode}</li>
-                      <li>Contact Person: ${req.body.contactperson}</li>
-                      <li>Name: ${req.body.name}</li>
-                      <li>Phone Number: ${req.body.phone_no}</li>
-                      <li>Type Of Cusines: ${req.body.type_of_cusines}</li>
-                      <li>Time-From: ${req.body.time_from}</li>
-                      <li>Time-To: ${req.body.time_to}</li>
-                      <li>Services(delivery): ${req.body.services1 ? true : false}</li>
-                      <li>Services(Walk-In): ${req.body.services2 ? true : false}</li>
-                      <li>Outlet Type(Cloud Kitchen): ${req.body.outlettype1 ? true : false}</li>
-                      <li>Outlet Type(Resturent): ${req.body.outlettype2 ? true : false}</li>
-                      <li>Outlet Type(Others): ${req.body.outlettype3 ? true : false}</li>
-                      <li>Outlet Type(If Others, Please specify): ${req.body.specify}</li>
-                    </ul>
-                  `;
-
-              var mailOptions = {
-                from: process.env.USER,
-                to: process.env.USER,
-                subject:'Adding services request',
-                text:'mail',
-                html: output,
-                attachments: [
-                  {
-                   path: path
                   }
-               ]
-              };
-              transporter.sendMail(mailOptions, function(error, info){
-                if (error) {
-                  console.log(error);
-                } else {
-                  console.log('Email sent: ' + info.response);
-                  fs.unlink(path,function(err){
-                    if(err){
-                        return res.end(err)
-                    }else{
-                        console.log("deleted")
-                    }
-                  })
-                }
+                });
               });
-        }
-    })
-})
+            
 
   
 module.exports = router;
